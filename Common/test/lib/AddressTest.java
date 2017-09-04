192e33ef718b320aff110b307ac71db466d50dcb
chaoukis <sammy.chaouki@dawsoncollege.qc.ca>
2017-09-03 21:19:07 -0400

    Create and test Address Class from Home

---
+++ Common/test/lib/AddressTest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package lib;
+
+/**
+ * @author katsuragi
+ *
+ */
+public class AddressTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Address myAddress = new Address("3040", "Sherbrooke");
+
+		System.out.println(myAddress.getAddress());
+
+	}
+
+}
